4c17f464198d68f5a24e2036bfeb7c48b136ab4b
hou dejun <dejun@visenze.com>
2018-06-21 10:04:23 +0800

    Create .stylelintrc.js

---
+++ css/.stylelintrc.js
@@ -0,0 +1,34 @@
+{
+  "extends": "stylelint-config-standard",
+  "plugins": [
+    "stylelint-order"
+  ],
+  "rules": {
+    "property-no-unknown": [
+      true,
+      {
+        "ignoreProperties": [
+          "composes"
+        ]
+      }
+    ],
+    "selector-pseudo-class-no-unknown": [
+      true,
+      {
+        "ignorePseudoClasses": [
+          "global",
+          "local"
+        ]
+      }
+    ],
+    "string-quotes": "single",
+    "order/order": [
+      "custom-properties",
+      "dollar-variables",
+      "declarations",
+      "at-rules",
+      "rules"
+    ],
+    "order/properties-order": []
+  }
+}
